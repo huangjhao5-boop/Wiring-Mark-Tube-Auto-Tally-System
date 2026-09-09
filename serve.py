5c04e471e3988d8164f3197368e5cb742c3c40b8
huangjhao5-boop <huangjhao5@gmail.com>
2026-09-09 17:14:28 +0900

    UP

---
+++ serve.py
@@ -0,0 +1,25 @@
+﻿import http.server
+import socketserver
+import os
+import sys
+
+PORT = 8080
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+if __name__ == '__main__':
+    socketserver.TCPServer.allow_reuse_address = True
+    with socketserver.TCPServer(("", PORT), NoCacheHTTPRequestHandler) as httpd:
+        print(f"Server started at http://localhost:{PORT} [NO-CACHE MODE]")
+        sys.stdout.flush()
+        httpd.serve_forever()
